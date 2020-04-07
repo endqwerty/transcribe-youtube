b5c599bc4b7c30ddac7d3e9626d86722e372b720
endqwerty <endqwerty@gmail.com>
2020-04-06 23:14:40 -0700

    make globally installable

--- watson.js
+++
@@ -1,5 +0,0 @@
-// watson.js
-const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1')
-const { IamAuthenticator } = require('ibm-watson/auth')
-var fs = require('fs')
-var path = require('path')
