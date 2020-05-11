2de38328dc17f3b711219e4f2743677e3b44bd9f
Daniel <endqwerty@gmail.com>
2020-05-10 20:10:53 -0700

    use google eslint

--- transcribe-youtube/fetch.js
+++ transcribe-youtube/fetch.js
@@ -1,17 +1,19 @@
-const fs = require('fs')
-const youtubedl = require('youtube-dl')
+import { createWriteStream } from 'fs'
+import youtubedl from 'youtube-dl'
 
-const video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA',
+const video = youtubedl(
+  'https://www.youtube.com/watch?v=EKbT0pQdQ2o',
   // Optional arguments passed to youtube-dl.
-  ['--format=18'],
+  ['--format=140'],
   // Additional options can be given for calling `child_process.execFile()`.
-  { cwd: __dirname })
+  { cwd: __dirname }
+)
 
 // Will be called when the download starts.
-video.on('info', function(info) {
+video.on('info', function (info) {
   console.log('Download started')
   console.log('filename: ' + info._filename)
   console.log('size: ' + info.size)
 })
 
-video.pipe(fs.createWriteStream('myvideo.mp4'))
+video.pipe(createWriteStream('audio.m4a'))
